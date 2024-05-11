8c2a9170e97f7bdaace4a566ffd445b85a6176d6
irumi <irumiabeywickrama@gmail.com>
2024-05-11 15:50:02 +0530

    Add every section of the web side(Webside structure)

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+const App = () => (
+  <main className="relative">
+    Nav 
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      popular products
+    </section>
+    <section className="padding">
+      super quality
+    </section>
+    <section className="padding-x py-10">
+      services
+    </section>
+    <section className="padding">
+      special offers
+    </section>
+    <section className="bg-pale-blue padding">
+      customer reviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      footer
+    </section>
+  </main>
+)
+export default App;
